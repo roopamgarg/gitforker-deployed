4495ca24b19c13cde08ed0b63d1daf91e822dc5e
roopam527 <roopamg777@gmail.com>
2019-04-14 22:18:45 +0530

    make it better

--- src/server/routes/invite.js
+++ src/server/routes/invite.js
@@ -3,7 +3,7 @@ module.exports = (app) =>{
     app.get('/api/send_invitation/:username/:email',(req,res)=>{
         let {email,username} = req.params;
         const subject = `Hey, ${username} You are invited to join Gitforker`
-        const message = `We are glad to tell you that one of your programming buddy invited you to join <a href="#">GitForker Platform</a>`
+        const message = `We are glad to tell you that one of your programming buddy invited you to join <a href="https://gitforker.herokuapp.com">GitForker Platform</a>`
         sendEmail(email,subject,message)
         res.status(200).json({
             message:"Email Successfully Send"
