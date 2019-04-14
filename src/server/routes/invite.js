434106f4739ee7d2fa5350574e83a11a158c147f
roopam527 <roopamg777@gmail.com>
2019-04-14 22:17:10 +0530

    make it better

--- src/server/routes/invite.js
+++ src/server/routes/invite.js
@@ -2,7 +2,6 @@ const sendEmail = require('../factory/sendEmail')
 module.exports = (app) =>{
     app.get('/api/send_invitation/:username/:email',(req,res)=>{
         let {email,username} = req.params;
-        email = "roopamg777@gmail.com";
         const subject = `Hey, ${username} You are invited to join Gitforker`
         const message = `We are glad to tell you that one of your programming buddy invited you to join <a href="#">GitForker Platform</a>`
         sendEmail(email,subject,message)
